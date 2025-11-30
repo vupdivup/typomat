fd5f2b109bfa289711a06ad6c634806ff2edb478
vupdivup <vnmatt2003@gmail.com>
2025-11-30 16:08:12 +0100

    feat: cache db connections by ID

--- internal/data/data.go
+++ internal/data/data.go
@@ -13,23 +13,36 @@ import (
 )
 
 const (
+	// batchSize is the number of records to process in a single batch
+	// operation.
 	batchSize = 100
 )
 
+// dbCache is a map of opened database connections keyed by their IDs.
+var dbCache = map[string]*gorm.DB{}
+
+// Token represents a token record in the database.
 type Token struct {
-	Path      string `gorm:"primaryKey"`
-	Value     string `gorm:"primaryKey"`
+	// Path is the path to the file from which the token was extracted.
+	Path string `gorm:"primaryKey"`
+	// Value is the token value.
+	Value string `gorm:"primaryKey"`
+
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
 
+// File represents a file in the user's file system.
 type File struct {
-	Path        string `gorm:"primaryKey"`
+	// Path is the path to the file.
+	Path string `gorm:"primaryKey"`
+	// Fingerprint is the file's hash fingerprint.
 	Fingerprint string
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
-	DeletedAt   gorm.DeletedAt `gorm:"index"`
+
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
 
 // UpsertTokens inserts or updates the given tokens in a database.
@@ -84,21 +97,33 @@ func GetTokens(dbId string) iter.Seq2[Token, error] {
 }
 
 // openDb opens (or creates) a database with the given identifier.
+// Cached database connections are reused.
 func openDb(id string) (*gorm.DB, error) {
+	if db, ok := dbCache[id]; ok {
+		return db, nil
+	}
+
+	// Hash the ID to create a filename
 	h := sha256.New()
 	h.Write([]byte(id))
 	hashedId := hex.EncodeToString(h.Sum(nil))
 
+	// Open (or create) the SQLite database
 	db, err := gorm.Open(sqlite.Open(hashedId+".db"), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
 
+	// Perform migrations
 	if err := db.AutoMigrate(&Token{}); err != nil {
 		return nil, err
 	}
 	if err := db.AutoMigrate(&File{}); err != nil {
 		return nil, err
 	}
+
+	// Cache the opened database
+	dbCache[id] = db
+
 	return db, nil
 }
