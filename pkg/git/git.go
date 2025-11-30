1d631144aacefbbd5aa9699b9fdcfc5e01bbd512
vupdivup <vnmatt2003@gmail.com>
2025-11-30 21:17:55 +0100

    feat: write ls-files abstraction

---
+++ pkg/git/git.go
@@ -0,0 +1,81 @@
+package git
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/sabhiram/go-gitignore"
+)
+
+// getGitignore locates the first .gitignore file in specified repository path,
+// returning an error if no such file is found.
+func getGitignore(repoPath string) (string, error) {
+	files, err := filepath.Glob(filepath.Join(repoPath, "*.gitignore"))
+	if err != nil {
+		return "", err
+	}
+
+	if len(files) == 0 {
+		return "", os.ErrNotExist
+	}
+
+	return files[0], nil
+}
+
+// LsFiles lists absolute file paths of all files in the specified repository
+// path, excluding those ignored by .gitignore. Only .gitignore files in the
+// root of the repository are considered.
+//
+// Note that the target directory is scanned regardless of whether it contains a
+// .git subdirectory. This means LsFiles returns files for any directory, not
+// just Git repositories.
+func LsFiles(repoPath string) ([]string, error) {
+	// Check for .gitignore file
+	gitignorePath, err := getGitignore(repoPath)
+	hasGitignore := err == nil
+
+	// Compile .gitignore if it exists
+	var gitignore *ignore.GitIgnore
+	if hasGitignore {
+		gitignore, err = ignore.CompileIgnoreFile(gitignorePath)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	files := []string{}
+	walk := func(path string, d os.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// Skip .git directory
+		if d.IsDir() && d.Name() == ".git" {
+			return filepath.SkipDir
+		} else if !d.IsDir() {
+			relPath, err := filepath.Rel(repoPath, path)
+			if err != nil {
+				return err
+			}
+
+			if !hasGitignore || !gitignore.MatchesPath(relPath) {
+				// Append absolute path
+				absPath, err := filepath.Abs(filepath.Join(repoPath, relPath))
+				if err != nil {
+					return err
+				}
+				files = append(files, absPath)
+			}
+		}
+
+		return nil
+	}
+
+	// Walk the repository directory
+	err = filepath.WalkDir(repoPath, walk)
+	if err != nil {
+		return nil, err
+	}
+
+	return files, nil
+}
