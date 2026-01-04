5db80f9a63aaf9961ad051458813434dc8f92b55
vupdivup <vnmatt2003@gmail.com>
2026-01-04 09:33:16 +0100

    consolidate error handling

--- cmd/typomat/main.go
+++ cmd/typomat/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/spf13/cobra"
 	"github.com/vupdivup/typomat/internal/config"
@@ -13,20 +12,31 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   fmt.Sprintf("%s <directory>", config.AppName),
 	Short: "Turn your code into muscle memory",
-	Long: `
-typomat is a command-line typing practice tool that creates exercises
+	Long: `typomat is a command-line typing practice tool that creates exercises
 from the contents of your repository.
 
 It runs through a directory's source code, extracting words from variable
 declarations, string literals and function signatures. These words are then used
 to build short, randomized typing prompts relevant to your codebase.
 
-Run typomat by passing the path to a directory you'd like to practice on.`,
+Run typomat by passing the path to the directory you'd like to practice on.`,
 	Args: cobra.ExactArgs(1),
 	RunE: run,
 }
 
 func run(cmd *cobra.Command, args []string) error {
+	// Configure application
+	if err := config.Init(); err != nil {
+		zap.S().Error("Failed to initialize configuration", "error", err)
+		return err
+	}
+
+	// Clean old files
+	// NOTE: cleanup errors don't block application startup
+	if err := config.RemoveOldFiles(); err != nil {
+		zap.S().Error("Failed to clean old files", "error", err)
+	}
+
 	// Handle purge flag
 	purge, err := cmd.Flags().GetBool("purge")
 	if err != nil {
@@ -46,41 +56,12 @@ func run(cmd *cobra.Command, args []string) error {
 }
 
 func init() {
-	rootCmd.SilenceErrors = true
-	rootCmd.SilenceUsage = true
-	rootCmd.Flags().BoolP(
-		"purge", "p", false, "purge application cache")
+	rootCmd.Flags().BoolP("purge", "p", false, "purge application cache")
 }
 
 func main() {
-	// Defer zap logger sync
 	defer zap.S().Sync() //nolint:errcheck
 
-	// Configure application
-	if err := config.Init(); err != nil {
-		zap.S().Error("Failed to initialize configuration", "error", err)
-		exitWithErr(err)
-	}
-
-	// Clean old files
-	// NOTE: cleanup errors don't block application startup
-	if err := config.RemoveOldFiles(); err != nil {
-		zap.S().Error("Failed to clean old files", "error", err)
-	}
-
 	// Run main command
-	if err := rootCmd.Execute(); err != nil {
-		exitWithErr(err)
-	}
-}
-
-// exitWithErr prints an error message to stderr along with the usage
-// instructions.
-// It then exits the application with a non-zero status code.
-func exitWithErr(err error) {
-	zap.S().Error("Application error", "error", err)
-	fmt.Fprintf(os.Stderr, "Error: %s\n\n", err.Error())
-	fmt.Fprint(os.Stderr, rootCmd.UsageString()+"\n")
-	zap.S().Sync() //nolint:errcheck
-	os.Exit(1)
+	rootCmd.Execute() // nolint:errcheck
 }
